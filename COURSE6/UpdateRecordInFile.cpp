ddc56fac6765f82ccb37f8af672a6c3060295695
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-03-03 09:39:46 +0800

    latest

---
+++ COURSE6/UpdateRecordInFile.cpp
@@ -0,0 +1,95 @@
+//TODO : print file content before and after deleting a record from it (Names.txt)
+
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void SaveVectorToFile(string FileName, vector <string> vFileContent)
+{
+  fstream MyFile;
+
+  MyFile.open(FileName,ios::out);
+
+  if(MyFile.is_open())
+  {
+      for (string &write : vFileContent)
+      {
+        if(write != "")
+        {
+          MyFile << write << endl;
+        }
+      }
+      MyFile.close();
+  }
+
+}
+
+void LoadFileDataToVector(string FileName, vector<string> &vFileContent)
+{
+  fstream MyFile;
+  
+  MyFile.open(FileName,ios::in);
+
+  if(MyFile.is_open())
+  {
+    string Line;
+
+    while(getline(MyFile,Line))
+    {
+       vFileContent.push_back(Line);
+    }
+    MyFile.close();
+  }
+
+}
+
+void PrintFileToConsole(string FileName)
+{
+   fstream MyFile;
+
+   MyFile.open(FileName,ios::in);
+
+   if(MyFile.is_open())
+   {
+    string Line;
+
+     while(getline(MyFile,Line))
+     {
+        cout<<Line<<endl;
+     }
+
+     MyFile.close();
+   }
+}
+
+void UpdateRecordInFile(string FileName, string Record , string Update)
+{
+  vector <string> vNames;
+  
+  LoadFileDataToVector(FileName,vNames);
+
+  for(string &x : vNames)
+  {
+     if(x==Record){
+        x = Update;
+     }
+  }
+
+  SaveVectorToFile(FileName,vNames);
+}
+
+int main()
+{
+  cout<<"File before deleting record : "<<endl;
+
+  PrintFileToConsole("Names.txt");
+
+  UpdateRecordInFile("Names.txt","ilyass","legend");
+
+  cout<<"File after updating record : "<<endl;
+
+  PrintFileToConsole("Names.txt");
+
+}
