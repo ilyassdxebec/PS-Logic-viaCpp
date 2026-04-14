4430f78171b3a28bb2f2695345883717a21d78e2
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-04-14 11:36:55 +0800

    update

--- OOP/Interfaces.cpp
+++ OOP/Interfaces.cpp
@@ -1,44 +1,104 @@
 #include<iostream>
 using namespace std;
 
-class clsMobile
+class clsLivingBeing
 {
  public:
 
-  virtual void Color(const string &Color) = 0;
-  virtual void Price(const float &Price) = 0;
-  virtual void Weight(const int &Weight) = 0;
+  virtual void Eat() = 0;
+  virtual void Sleep() = 0;
+  virtual void Die() = 0;
+  virtual void Born() = 0;
+  virtual void Survive() = 0;
 
 };
 
-class clsIphone : public clsMobile
+class clsHuman : public clsLivingBeing
 {
-
  public:
+ 
+  void Eat()
+  {
+    cout<<"Every Human Has To Eat!\n";
+  }
 
-    void Color(const string &Color)
-    {
+  void Sleep()
+  {
+    cout<<"Every Human Has To Sleep!\n";
 
-    }
+  }
 
-    void Price(const float &Price)
-    {
+  void Die()
+  {
+    cout<<"Every Human Has To Die!\n";
 
-    }
+  }
 
-    void Weight(const int &Weight)
-    {
+  void Born()
+  {
+    cout<<"Every Human Is born!\n";
 
-    }
+  }
 
+  void Survive()
+  {
+    cout<<"Every Human Strive to Survive!\n\n";
+
+  }
+};
+
+class clsLion : public clsLivingBeing
+{
+ public:
+ 
+  void Eat()
+  {
+    cout<<"Every Lion Has To Eat!\n";
+  }
+
+  void Sleep()
+  {
+    cout<<"Every Lion Has To Sleep!\n";
+
+  }
+
+  void Die()
+  {
+    cout<<"Every Lion Has To Die!\n";
+
+  }
+
+  void Born()
+  {
+    cout<<"Every Lion Is born!\n";
+
+  }
+
+  void Survive()
+  {
+    cout<<"Every Lion Strive to Survive!\n\n";
+
+  }
 };
 
 int main()
 {
-     
- clsIphone Iphone1;
 
- cout<<"Hello loser";
+ clsHuman Human1;
+
+ Human1.Die();
+ Human1.Sleep();
+ Human1.Born();
+ Human1.Eat();
+ Human1.Survive();
+
+ clsLion Lion1;
+
+ Lion1.Die();
+ Lion1.Sleep();
+ Lion1.Born();
+ Lion1.Eat();
+ Lion1.Survive();
 
  system("pause>0");
 
